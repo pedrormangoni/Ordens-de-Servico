30e1e586f09fafef51ef31b083a3407a499b6f43
Pedro Augusto Ribeiro Mangoni <pedroaugustoribeiromangoni@gmail.com>
2026-05-24 22:11:50 -0300

    add ClientEntity

---
+++ src/main/java/com/upf/trabalhofinal/controller/ClientController.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.upf.trabalhofinal.controller;
+
+import com.upf.trabalhofinal.entity.ClientEntity;
+import jakarta.ejb.EJB;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Pedro
+ */
+@Named(value = "clientController")
+@SessionScoped
+public class ClientController implements Serializable {
+    
+    @EJB 
+    private ClientEntity client = new ClientEntity();
+    
+    private List<ClientEntity> pessoaList = new ArrayList<>();
+    
+    private ClientEntity selected;
+    
+    public ClientEntity getSelected() {
+        return selected;
+    }
+    
+    public void setSelected(ClientEntity selected) {
+        this.selected = selected;
+    }
+    
+    
+}
---
+++ src/main/java/com/upf/trabalhofinal/entity/ClientEntity.java
@@ -0,0 +1,133 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.upf.trabalhofinal.entity;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Pedro
+ */
+@Entity
+@Table(name="clients") 
+public class ClientEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional=false)
+    @Column(name="id")
+    private Integer id;
+    
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Basic(optional=false)
+    @Column(name="name")
+    private String name;
+
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Basic(optional=false)
+    @Column(name="document")   
+    private String document;
+    
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Basic(optional=false)
+    @Column(name="´phone")
+    private String phone; 
+    
+    @Size(min = 1, max = 500)
+    @Column(name="´email")
+    private String email;
+    
+    @Size(min = 1, max = 500)
+    @Column(name="´adress")
+    private String adress;
+    
+    // GETTER E SETTERS
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ClientEntity)) {
+            return false;
+        }
+        ClientEntity other = (ClientEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.upf.trabalhofinal.entity.ClientEntity[ id=" + id + " ]";
+    }
+    
+}
